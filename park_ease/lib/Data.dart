2f4b8678967879d038ab70d9b7c0efce7df78869
tavva-srinivas <127462909+tavva-srinivas@users.noreply.github.com>
2024-05-18 23:32:16 +0530

    Delete park_ease directory

--- park_ease/lib/Data.dart
+++
@@ -1,5 +0,0 @@
-const collegeLogo = 'assets/images/tit.png';
-const profilePath = "assets/images/profile.jpg";
-const LeaderName = "Mr. Nitish Roy";
-const GoogleMapAPiKey = "AIzaSyBcbAQMDBEJ31E05b0_guLMO96IGpLBzdI";
-const bio = "Hello My name is Nitish Roy and I am a Flutter Developer this project is made by me and my team members this project is made for the College project, if you want to learn how to develope this peoject the you can follow my youtube channe FLUTTERHERO";
